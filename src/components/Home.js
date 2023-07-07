f31a379fd41d5b48beb99c030873e1daa2ce586f
prashant9315 <137028444+prashant9315@users.noreply.github.com>
2023-07-07 14:27:36 +0530

    ADD:image slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
-import View from "./View";
+import Category from "./Category";
 
 function Home() {
   return (
     <Container>
       <ImgSlider />
-      <View />
+      <Category />
     </Container>
   );
 }
